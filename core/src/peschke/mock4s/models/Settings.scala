931a0268b818366b38badc3356fec07e2fb78487
Morgen Peschke <morgen.peschke@gmail.com>
2023-04-10 17:21:59 -0700

    Remove unused wrapping around settings json

--- core/src/peschke/mock4s/models/Settings.scala
+++ core/src/peschke/mock4s/models/Settings.scala
@@ -1,15 +1,15 @@
 package peschke.mock4s.models
 
-import io.circe.{Decoder, Encoder, Json}
+import io.circe.{Decoder, Encoder}
 import io.circe.syntax._
 import peschke.mock4s.utils.Circe._
 
 final case class Settings(mocks: List[MockDefinition])
 object Settings {
   implicit val decoder: Decoder[Settings] = accumulatingDecoder { c =>
-    c.downField("mocks").asAcc[List[MockDefinition]].map(Settings.apply)
+    c.asAcc[List[MockDefinition]].map(Settings.apply)
   }
   implicit val encoder: Encoder[Settings] = Encoder.instance { settings =>
-    Json.obj("mocks" := settings.mocks)
+    settings.mocks.asJson
   }
 }
